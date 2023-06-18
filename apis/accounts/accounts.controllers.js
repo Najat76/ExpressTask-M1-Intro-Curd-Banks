15e0c4e5ba9c865d701faffb504da63881148c54
Najat76 <safari76@hotmail.co.uk>
2023-06-18 11:27:29 +0300

    I had err in app.js

--- apis/accounts/accounts.controllers.js
+++ apis/accounts/accounts.controllers.js
@@ -1,3 +1,59 @@
-(req, res) => {
+let accounts = require("../../accounts");
+
+//Fetch Accounts (get all accounts)
+exports.getAllAcoounts = (req, res) => {
   return res.status(200).json(accounts);
 };
+
+//Create Account
+//https://www.freecodecamp.org/news/how-to-get-the-last-item-in-an-array-in-javascript/
+//to get the last item in the array : const lastItem = accounts[accounts.length - 1]
+// newId : accounts.value(accounts.length - 1) <== wrong
+exports.createAccount = (req, res) => {
+  const newId = accounts[accounts.length - 1].id + 1;
+  let newAccount = req.body;
+  newAccount = accounts.push({ id: newId, ...newAccount, funds: 0 }); //...newAccount is spread operator
+  return res.status(201).json({ body: accounts });
+};
+
+// Delete Account
+//https://www.educative.io/answers/what-is-object-destructuring-in-javascript
+exports.deleteAccount = (req, res) => {
+  const { accountId } = req.params;
+  accounts = accounts.filter((account) => account.id !== accountId);
+  //console.log("accounts", accounts);
+  return res.status(204).json({ msg: "Account deleted" });
+};
+
+//Update Account
+exports.updateAccount = (req, res) => {
+  const { accountId } = req.params;
+  //accountFounded = accounts.find((account)=> accountId !== account.id);
+  const account = accounts.find((acc) => accountId == acc.id);
+  if (!account) {
+    return res.status(404).json({ msg: "Account not found" });
+  }
+  account.username = req.body.username ? req.body.username : account.username;
+  account.funds = req.body.funds ? req.body.funds : account.funds;
+  return res.status(200).json(account);
+};
+
+//Bonus: Create a route that retrieve a single account by the username of the owner.
+// exports.getOneAccount = (req, res) => {
+//   const { AccountUsername } = req.params;
+//   const account = accounts.find((acc) => accountUsername == acc.username);
+//   return res.status(200).json(account);
+//   if (!account) {
+//     return res.status(404).json({ msg: "Account not found" });
+//   }
+// });
+
+// module.exports = {
+//   getAllAcoounts,
+//   createAccount,
+//   deleteAccount,
+//   updateAccount,
+//   getOneAccount,
+// };
+// if am not using exports.functionName, then i have to use const functionName ()=> {} scenario
+//and call at the buttom moudule.exports{conatins functions name seperated by ,}
--- apis/accounts/accounts.routes.js
+++ apis/accounts/accounts.routes.js
@@ -1,37 +1,30 @@
 //https://expressjs.com/en/guide/routing.htmls
 const express = require("express");
 const router = express.Router();
+const {
+  getAllAcoounts,
+  createAccount,
+  deleteAccount,
+  updateAccount,
+  getOneAccount,
+} = require("./apis/accounts/accounts.controllers");
 let accounts = require("../../accounts"); // i need accounts.js
 
 // middleware that is specific to this router
 router.use();
 
-//app.someMethod(routePath, someHandler);
-//get all accounts
-router.get("/api/accounts", callback_function);
-
-//
+//Fetch Accounts (get all accounts)
+router.get("/", getAllAcoounts);
 //Create Account
-// router.post('/', (req, res) => {
-//     res.send('Got a POST request')
-//   })
+router.post("/", createAccount);
 
-// app.post("/acounts", (req, res) => {
-//   const newId = accounts.find(() => {});
-//   const newAccount = [];
-//   if (newAccount) {
-//   } else {
-//   }
-// });
+// Delete Account
 
-//Delete Account
-// router.delete('/accounts/:accountId', (req, res) => {
-//     res.send('Got a DELETE request at /user')
-//   })
+router.delete("/:accountId", deleteAccount);
 
 //Update Account
-// router.put('/accounts/:accountId`, (req, res) => {
-//     res.send('Got a PUT request at /user')
-//   })
+router.put("/", updateAccount);
 
+//Bonus: Create a route that retrieve a single account by the username of the owner.
+// router.get("/", getOneAccount);
 module.exports = router;
